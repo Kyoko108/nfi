3700c3a69b02b02796ea28f66a3e45aece366a5e
Kyoko <gatomghosh@gmail.com>
2022-12-25 12:11:25 +0530

    commit 2

--- src/Home.js
+++ src/Home.js
@@ -6,19 +6,28 @@ function Home() {
   return (
     <div>
       <Tab.Group>
-        <Tab.List className="tab_body">
-          <Tab>
-            <a href="/">About</a>{" "}
+        <Tab.List
+          className=" tab_body  flex space-x-0.5 rounded-xl bg-blue-900/20 p-1 bg-gradient-to-tr
+    from-pink-500
+    via-red-400
+    to-blue-500
+    background-animate shadow-lg"
+        >
+          <Tab className="px-4 py-2 font-medium leading-5 text-gray-700 transition ease-in-out margin-3 text-md duration-450 hover:text-gray-500 focus:outline-none focus:border-blue-600 focus:shadow-outline-blue active:text-gray-500">
+            Home
           </Tab>
-          &emsp;
-          <Tab>
-            <a href="/">Facilities</a>{" "}
+          <Tab className="px-4 py-2 font-medium leading-5 text-gray-700 transition ease-in-out margin-3 text-md duration-450 hover:text-white-600 focus:outline-none focus:border-blue-600 focus:shadow-outline-blue active:text-gray-500">
+            About
           </Tab>
-          &emsp;
-          <Tab>
-            <a href="/">About</a>{" "}
+          <Tab className="px-4 py-2 font-medium leading-5 text-gray-700 transition ease-in-out margin-3 text-md duration-450 hover:text-white-600 focus:outline-none focus:border-blue-600 focus:shadow-outline-blue active:text-gray-500">
+            Faculty
           </Tab>
-          &emsp;
+          <Tab className="px-4 py-2 font-medium leading-5 text-gray-700 transition ease-in-out margin-3 text-md duration-450 hover:text-white-600 focus:outline-none focus:border-blue-600 focus:shadow-outline-blue active:text-gray-500">
+            Contact
+          </Tab>
+          <Tab className="px-4 py-2 font-medium leading-5 text-gray-700 transition ease-in-out margin-3 text-md duration-450 hover:text-white-600 focus:outline-none focus:border-blue-600 focus:shadow-outline-blue active:text-gray-500">
+            Payments
+          </Tab>
         </Tab.List>
       </Tab.Group>
     </div>
