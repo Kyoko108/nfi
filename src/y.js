a421ce4d4ade6ef1c6dc40667d0a07a8ec697790
Kyoko <gatomghosh@gmail.com>
2023-02-26 20:16:32 +0530

    hello commit

--- src/y.js
+++ src/y.js
@@ -1,11 +1,17 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const PoRT = process.env.PORT || 5000;
 
 const app = express();
 
+const a = path.join(__dirname, "../src/index.js");
+app.use(express.static(a));
+
 app.get("/", (req, res) => {
-  res.send("Hi I am live");
+  res.send(a);
 });
 
 const start = async () => {
